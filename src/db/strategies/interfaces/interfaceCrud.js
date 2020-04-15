b2ccc1c5e2ab1b53275eb95e2c6a5da7a6ad2e40
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2020-04-15 01:22:42 -0300

    Base arquiteture strategy

---
+++ src/db/strategies/interfaces/interfaceCrud.js
@@ -0,0 +1,27 @@
+class NotImplementedExepction extends Error {
+    constructor() {
+        super("Not Implemented Excepction")
+    }
+}
+
+class ICrud {
+
+    create(item) {
+        throw new NotImplementedExepction();
+    }
+
+    read(query) {
+       throw new NotImplementedExepction();
+    }
+
+    update(id, item) {
+        throw new NotImplementedExepction();
+    }
+
+    delete(id) {
+        throw new NotImplementedExepction();
+    }
+
+}
+
+module.exports = ICrud;
